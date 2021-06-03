b24ecde5aec06625d147f861beb03df1010c5c9a
HungryNuggets <web2020.audrey@gmail.com>
2021-06-03 09:34:50 +0200

    projects

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Projects;
+use App\Repository\ProjectsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,20 +13,15 @@ class HomeController extends AbstractController
 
     /**
      * @Route("/", name="home")
+     * @param ProjectsRepository $projectsRepository
+     * @return Response
      */
-    public function home()
+    public function home(ProjectsRepository $projectsRepository)
     {
-        $project = new Projects();
-        $project->setTitle('Title');
-        $project->setDescription('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus laoreet tincidunt odio. Fusce imperdiet cursus felis a lacinia. Quisque sagittis finibus ex, at faucibus mi ornare quis. Duis eu purus vitae libero rutrum blandit.');
-        $project->setUrl("https://url.com");
-        $project->setImgName("https://via.placeholder.com/250x100/0E0F0B/656E40?Text=Placeholder");
-        $project->setImgAlt("alt img");
-        $project->setImgTitle("title img");
 
         return $this->render("public/home_portfolio.html.twig",[
             "permission"=> "public",
-            "project"=>$project
+            "projects"=>$projectsRepository->findAll()
         ]);
     }
 
---
+++ src/Entity/Projets.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjetsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProjetsRepository::class)
+ */
+class Projets
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
